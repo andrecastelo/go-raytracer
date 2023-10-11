801b78a9fb4de4bc55c46201d11058f6c5264679
Andre Castelo <andrecastelo@gmail.com>
2023-10-10 22:32:30 -0300

    feat: vector creation, addition and subtraction

--- internal/tuple/tuple.go
+++ internal/tuple/tuple.go
@@ -6,5 +6,27 @@ type Tuple struct {
 }
 
 func Point(x float64, y float64, z float64) *Tuple {
+	return &Tuple{x: x, y: y, z: z, w: 1}
+}
+
+func Vector(x float64, y float64, z float64) *Tuple {
 	return &Tuple{x: x, y: y, z: z, w: 0}
+}
+
+func (t *Tuple) Add(t2 *Tuple) *Tuple {
+	return &Tuple{
+		x: t.x + t2.x,
+		y: t.y + t2.y,
+		z: t.z + t2.z,
+		w: t.w + t2.w,
+	}
+}
+
+func (t *Tuple) Subtract(t2 *Tuple) *Tuple {
+	return &Tuple{
+		x: t.x - t2.x,
+		y: t.y - t2.y,
+		z: t.z - t2.z,
+		w: t.w - t2.w,
+	}
 }
--- internal/tuple/tuple_test.go
+++ internal/tuple/tuple_test.go
@@ -13,7 +13,55 @@ type TupleSuite struct {
 func (suite *TupleSuite) TestPointIsCreatedCorrectly() {
 	point := Point(1, 2, 3)
 
-	suite.Equal(point.w, 0)
+	suite.Equal(point.w, 1)
+	suite.Equal(point.x, 1.0)
+	suite.Equal(point.y, 2.0)
+	suite.Equal(point.z, 3.0)
+}
+
+func (suite *TupleSuite) TestVectorIsCreatedCorrectly() {
+	vector := Vector(4, 5, 6)
+
+	suite.Equal(vector.w, 0)
+	suite.Equal(vector.x, 4.0)
+	suite.Equal(vector.y, 5.0)
+	suite.Equal(vector.z, 6.0)
+}
+
+func (suite *TupleSuite) TestTupleAddition() {
+	point := Point(3, -2, 5)
+	vector := Vector(-2, 3, 1)
+
+	newPoint := point.Add(vector)
+
+	suite.Equal(newPoint.w, 1)
+	suite.Equal(newPoint.x, 1.0)
+	suite.Equal(newPoint.y, 1.0)
+	suite.Equal(newPoint.z, 6.0)
+}
+
+func (suite *TupleSuite) TestSubtractingTwoPoints() {
+	pointA := Point(3, 2, 1)
+	pointB := Point(5, 6, 7)
+
+	vector := pointA.Subtract(pointB)
+
+	suite.Equal(vector.w, 0)
+	suite.Equal(vector.x, -2.0)
+	suite.Equal(vector.y, -4.0)
+	suite.Equal(vector.z, -6.0)
+}
+
+func (suite *TupleSuite) TestSubtractingAVectorFromAPoint() {
+	point := Point(3, 2, 1)
+	vector := Vector(5, 6, 7)
+
+	newVector := point.Subtract(vector)
+
+	suite.Equal(newVector.w, 1)
+	suite.Equal(newVector.x, -2.0)
+	suite.Equal(newVector.y, -4.0)
+	suite.Equal(newVector.z, -6.0)
 }
 
 func TestTupleSuite(t *testing.T) {
