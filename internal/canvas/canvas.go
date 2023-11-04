5e7369ca27cf4bc9b843e949ba629b1dc2f25039
Andre Castelo <andrecastelo@gmail.com>
2023-11-03 23:40:57 -0300

    feat: add method to convert canvas to ppm

--- internal/canvas/canvas.go
+++ internal/canvas/canvas.go
@@ -2,19 +2,75 @@ package canvas
 
 import (
 	"andrecastelo/raytracer/internal/tuple"
+	"fmt"
 )
 
+// Canvas is a struct that will hold a matrix of tuple.Tuple.
 type Canvas struct {
+    width  int
+    height int
     pixels [][]tuple.Tuple
 } 
 
-func MakeCanvas(size int) *Canvas {
+func MakeCanvas(width int, height int) *Canvas {
     canvas := &Canvas{}
 
-    canvas.pixels = make([][]tuple.Tuple, size)
-    for i := 0; i < size; i++ {
-        canvas.pixels[i] = make([]tuple.Tuple, size)
+    canvas.width = width
+    canvas.height = height
+    canvas.pixels = make([][]tuple.Tuple, width)
+    for i := 0; i < width; i++ {
+        canvas.pixels[i] = make([]tuple.Tuple, height)
     }
 
     return canvas
+}
+
+func clamp(value int, max_value int, min_value int) int {
+    if value < min_value {
+        return min_value
+    } else if value > max_value {
+        return max_value
+    } else {
+        return value
+    }
+}
+
+func (canvas *Canvas) WritePixel(x int, y int, color *tuple.Tuple) *Canvas {
+    canvas.pixels[x][y] = *color
+
+    return canvas
+}
+
+func (canvas *Canvas) PpmArray() []string  {
+    var ppm []string
+
+    ppm = append(ppm, "P3")
+    ppm = append(ppm, fmt.Sprintf("%d %d", canvas.width, canvas.height))
+    ppm = append(ppm, "255")
+
+    for _, line := range canvas.pixels {
+        for _, pixel := range line {
+            array := pixel.Array()
+			ir := clamp(int(255.999 * array[0]), 255, 0)
+			ig := clamp(int(255.999 * array[1]), 255, 0)
+			ib := clamp(int(255.999 * array[2]), 255, 0)
+
+            line := fmt.Sprintf("%d %d %d", ir, ig, ib)
+            ppm = append(ppm, line)
+        }
+    }
+
+    return ppm
+}
+
+func (canvas *Canvas) Ppm() string {
+    var ppmString string
+    ppm := canvas.PpmArray()
+
+    for _, line := range ppm {
+        ppmString += line
+        ppmString += "\n"
+    }
+
+    return ppmString
 }
--- internal/canvas/canvas_test.go
+++ internal/canvas/canvas_test.go
@@ -2,6 +2,7 @@ package canvas
 
 import (
 	// "andrecastelo/raytracer/internal/tuple"
+	"andrecastelo/raytracer/internal/tuple"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -12,16 +13,71 @@ type CanvasSuite struct {
 }
 
 func (suite *CanvasSuite) TestCanvasCreation() {
-    canvas := MakeCanvas(32)
+    canvas := MakeCanvas(32, 16)
 
+    suite.Equal(canvas.width, 32)
+    suite.Equal(canvas.height, 16)
     suite.Len(canvas.pixels, 32)
 
     for _, line := range canvas.pixels {
-        suite.Len(line, 32)
+        suite.Len(line, 16)
     } 
 
     firstPixel := canvas.pixels[0][0]
 	suite.Equal(firstPixel.Array(), [...]float64{0.0, 0.0, 0.0})
+}
+
+func (suite *CanvasSuite) TestWritingPixelToCanvas() {
+    canvas := MakeCanvas(10, 10)
+    red := tuple.Color(1.0, 0.0, 0.0)
+
+    canvas.WritePixel(2, 3, red)
+
+    suite.Equal(canvas.pixels[2][3].Array(), [...]float64{1.0, 0.0, 0.0})
+}
+
+func (suite *CanvasSuite) TestCanvasPpmArray() {
+    canvas := MakeCanvas(5, 3)
+    c1 := tuple.Color(1.5, 0.0, 0.0)
+    c2 := tuple.Color(0.0, 0.5, 0.0)
+    c3 := tuple.Color(-0.5, 0.0, 1.0)
+    canvas.WritePixel(0, 0, c1)
+    canvas.WritePixel(2, 1, c2)
+    canvas.WritePixel(4, 2, c3)
+    ppm := canvas.PpmArray()
+    
+    suite.Equal("P3", ppm[0])
+    suite.Equal("5 3", ppm[1])
+    suite.Equal("255", ppm[2])
+    suite.Equal("255 0 0", ppm[3])
+    suite.Equal("0 127 0", ppm[10])
+    suite.Equal("0 0 255", ppm[17])
+}
+
+func (suite *CanvasSuite) TestCanvasPpmString() {
+    canvas := MakeCanvas(3, 3)
+    c1 := tuple.Color(1.0, 0.0, 0.0)
+    c2 := tuple.Color(0.0, 0.5, 0.0)
+    c3 := tuple.Color(-0.5, 0.0, 1.0)
+    canvas.WritePixel(0, 0, c1)
+    canvas.WritePixel(1, 0, c2)
+    canvas.WritePixel(2, 0, c3)
+    ppm := canvas.Ppm()
+
+    expected := `P3
+3 3
+255
+255 0 0
+0 0 0
+0 0 0
+0 127 0
+0 0 0
+0 0 0
+0 0 255
+0 0 0
+0 0 0
+`
+    suite.Equal(expected, ppm)
 }
 
 func TestCanvasSuite(t *testing.T) {
