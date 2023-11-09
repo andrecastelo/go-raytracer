1f693c432de76af0cc2f2fc65e9f3982d5f68f9b
Andre Castelo <andrecastelo@gmail.com>
2023-11-09 09:29:25 -0300

    feat: matrix boilerplate

---
+++ internal/matrix/matrix.go
@@ -0,0 +1,20 @@
+package matrix
+
+import "errors"
+
+type Matrix = [][]float64
+
+func MakeMatrix(elements [][]float64) (*Matrix, error) {
+    height := len(elements)
+    width := len(elements[0])
+
+    for i := 0; i < height; i ++ {
+        if len(elements[i]) != width {
+            return nil, errors.New("Matrix does not have the same width for all rows")
+        }
+    }
+
+    return &elements, nil
+}
+
+
