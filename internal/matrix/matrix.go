18f60c51afeededcb3c7effbfdfa3f82101cb5df
Andre Castelo <andrecastelo@gmail.com>
2023-11-11 15:39:53 -0300

    add some tests for matrix creation

--- internal/matrix/matrix.go
+++ internal/matrix/matrix.go
@@ -4,17 +4,15 @@ import "errors"
 
 type Matrix = [][]float64
 
-func MakeMatrix(elements [][]float64) (*Matrix, error) {
-    height := len(elements)
-    width := len(elements[0])
+func MakeMatrix(elements [][]float64) (Matrix, error) {
+	height := len(elements)
+	width := len(elements[0])
 
-    for i := 0; i < height; i ++ {
-        if len(elements[i]) != width {
-            return nil, errors.New("Matrix does not have the same width for all rows")
-        }
-    }
+	for i := 0; i < height; i++ {
+		if len(elements[i]) != width {
+			return nil, errors.New("Matrix does not have the same width for all rows")
+		}
+	}
 
-    return &elements, nil
+	return elements, nil
 }
-
-
--- internal/matrix/matrix_test.go
+++ internal/matrix/matrix_test.go
@@ -1,1 +1,74 @@
 package matrix
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type MatrixSuite struct {
+	suite.Suite
+}
+
+func (suite *MatrixSuite) MatrixesAreEqual(a [][]float64, b [][]float64) {
+	for i := range a {
+		for j := range a[i] {
+			suite.Equal(a[i][j], b[i][j])
+		}
+	}
+}
+
+func (suite *MatrixSuite) TestCanCreateMatrix() {
+	twoByTwo := [][]float64{
+		{-3.0, 5.0},
+		{1.0, -2.0},
+	}
+
+	twoByTwoMatrix, err := MakeMatrix(twoByTwo)
+
+	suite.NotNil(twoByTwoMatrix)
+	suite.Nil(err)
+	suite.MatrixesAreEqual(twoByTwo, twoByTwoMatrix)
+
+	threeByThree := [][]float64{
+		{-3.0, 5.0, 0.0},
+		{1.0, -2.0, -7.0},
+		{0.0, 1.0, 1.0},
+	}
+
+	threeByThreeMatrix, err := MakeMatrix(threeByThree)
+
+	suite.NotNil(threeByThreeMatrix)
+	suite.Nil(err)
+	suite.MatrixesAreEqual(threeByThree, threeByThreeMatrix)
+
+	fourByFour := [][]float64{
+		{-3.0, 5.0, 0.0, 1.0},
+		{1.0, -2.0, -7.0, 8.5},
+		{0.0, 1.0, 1.0, 16.0},
+		{13.0, 14.0, 15.0, 16.0},
+	}
+
+	fourByFourMatrix, err := MakeMatrix(fourByFour)
+
+	suite.NotNil(fourByFourMatrix)
+	suite.Nil(err)
+	suite.MatrixesAreEqual(fourByFour, fourByFourMatrix)
+}
+
+func (suite *MatrixSuite) TestMatrixCreationFailure() {
+	invalid := [][]float64{
+		{1.0, 2.0, 3.0, 4.0},
+		{5.0, 6.0, 7.0},
+		{8.0, 9.0, 10.0, 11.0},
+		{12.0, 13.0, 14.0, 15.0},
+	}
+
+	invalidMatrix, err := MakeMatrix(invalid)
+
+	suite.Nil(invalidMatrix)
+	suite.NotNil(err)
+}
+
+func TestMatrixSuite(t *testing.T) {
+	suite.Run(t, new(MatrixSuite))
+}
